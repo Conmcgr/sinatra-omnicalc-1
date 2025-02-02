18a2e0a620b845f567ffb78bfc7c2595be37b735
Connor McGraw <42752783+Conmcgr@users.noreply.github.com>
2025-02-02 04:42:20 +0000

    basic set up

--- app.rb
+++ app.rb
@@ -2,8 +2,21 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:homepage)
+end
+
+get("/square/new") do
+  erb(:homepage)
+end
+
+get("/square_root/new") do
+  erb(:homepage)
+end
+
+get("/random/new") do
+  erb(:homepage)
+end
+
+get("/payment/new") do
+  erb(:homepage)
 end
